22f13235b387c6986df550a5172c46ed87e48047
Ali Gencay <98914030+aligencay@users.noreply.github.com>
2022-10-24 21:15:35 +0300

    Numeric Value Operations

---
+++ NumericValueOperationsSQL.sql
@@ -0,0 +1,26 @@
+use Northwind
+
+--sayýsal deðer iþlemleri
+
+select 3+2
+select 3*3
+select 4/2
+select 9-7
+
+-- PI : pi sayýsýný verir
+select PI()
+
+-- SIN : sinüs alýr
+select SIN(PI())
+
+--POWER : üs alýr
+select POWER(2,3)
+
+--ABS : mutlak deðer alýr
+select ABS(-12)
+
+--RAND : 0 - 1 arasýnda rastgele sayý üretir
+select RAND()
+
+--FLOOR : yuvarlama yapar
+select FLOOR(RAND()*100)
